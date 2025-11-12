07e2f9654ef12791e430ff5a33f3df0a9cae2acf
Alex Rocha <alex.diego.rocha@gmail.com>
2025-11-12 00:16:49 -0300

    Incluido SistemaContext

---
+++ Feitep.MySql.Repository/SistemaContext.cs
@@ -0,0 +1,9 @@
+﻿using Feitep.MySql.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Feitep.MySql.Repository;
+
+public class SistemaContext : DbContext
+{
+    public DbSet<Cliente>? Clientes { get; set; }
+}
